746c9784229a03501e467a7bbc1b1bf7408ec9c1
DreamNew <dreamnew@gmail.com>
2026-07-25 17:59:44 +0530

    git add . git commit -m "Added phone validation and login UI" git push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlin {
+        jvmToolchain(11)
+    }
     buildFeatures {
         compose = true
     }
